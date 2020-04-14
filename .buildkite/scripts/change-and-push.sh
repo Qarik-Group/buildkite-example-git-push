202fd3a16602826cdda8f6acedf522bb566eaf20
Dr Nic Williams <drnicwilliams@gmail.com>
2020-04-15 07:55:26 +1000

    commit + push

--- .buildkite/scripts/change-and-push.sh
+++ .buildkite/scripts/change-and-push.sh
@@ -5,3 +5,16 @@ set -eu
 echo "--- :date: bump datestamp"
 
 date +%s > datestamp
+
+echo "--- :git: git commit"
+
+[[ -n "${BUILDKITE_BUILD_CREATOR_EMAIL}" ]] && {
+  git config --global user.email "${BUILDKITE_BUILD_CREATOR_EMAIL}"
+  git config --global user.name  "${BUILDKITE_BUILD_CREATOR}"
+}
+
+git add datestamp
+git commit -m "Bump datestamp during CI"
+
+echo "--- :git: git push"
+git push
