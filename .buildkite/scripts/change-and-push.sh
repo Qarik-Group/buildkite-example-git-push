58e8f720c434370c03baeffc0d1a8031b2a16f15
Dr Nic Williams <drnicwilliams@gmail.com>
2020-04-15 07:56:37 +1000

    add missing git config

--- .buildkite/scripts/change-and-push.sh
+++ .buildkite/scripts/change-and-push.sh
@@ -12,6 +12,7 @@ echo "--- :git: git commit"
   git config --global user.email "${BUILDKITE_BUILD_CREATOR_EMAIL}"
   git config --global user.name  "${BUILDKITE_BUILD_CREATOR}"
 }
+git config --global --get push.default || git config --global push.default simple
 
 git add datestamp
 git commit -m "Bump datestamp during CI"
